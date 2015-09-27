1d0c620f87440b366f21118521e149f80cd1205b
am503 <aliki_m@me.com>
2015-09-27 23:04:00 +0100

    completed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion finds use in many practical applications. For optimization problems involving
+# large matrices, matrix-inversion can be very computationally expensive. It is important therefore to
+# cache time-consuming computations to avoid computing them repeatedly. The following two functions cache
+# the inverse of a matrix, given that the matrix is invertible.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list which contains functions to:
+## 1. set the value of the matrix to be inverted
+## 2. get  the value of the matrix to be inverted
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -14,11 +20,19 @@ makeCacheMatrix <- function(x = matrix()) {
    setinverse<-function(solve) inverse<<-solve
    getinverse<-function()inverse
    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
-   
 }
 
+# ------------------------------------------------------------------
 
-## Write a short comment describing this function
+# To use, assign a variable, z, to the list obtained in makeCacheMatrix:
+#   z<-makeCacheMatrix(mat) where mat is the nonsingular matrix 
+# Typing  z$get() returns the matrix which was given as input.
+
+# ------------------------------------------------------------------
+
+# The second function takes as an input the list of functions created using makeCacheMatrix. The function
+# first checks to see if the matrix inverse has already been computed; if it has, it returns the inverse.
+# If not, it computes it and returns its value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,3 +46,13 @@ cacheSolve <- function(x, ...) {
   x$setinverse(inverse)
   inverse
 }
+
+# ------------------------------------------------------------------
+
+# Example: 
+# vec<-c(1,0,5,2,1,6,3,4,0)
+# A<- matrix(vec,nrow=3)
+# z<-makeCacheMatrix(A)
+# cacheSolve(z)
+
+# ------------------------------------------------------------------
